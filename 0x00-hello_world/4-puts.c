84eea399945e44ae4fe7e37479d6f6120f2f9cc3
Kimani3050 <cerikimani@gmail.com>
2022-03-10 23:45:25 +0300

    Wrote a C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,7 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{put("\" Programming is like building a multilingual puzzle")
+{
+put("\" Programming is like building a multilingual puzzle")
 return (0);
 }
